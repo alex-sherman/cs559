034688b91015d89c556568cfc072a05a09d06cc0
Alex Sherman <asherman1024@gmail.com>
2015-10-24 03:19:36 -0400

    Fixed canvas size, FPS counter and grass shader

--- Shaders/grass.fs
+++ Shaders/grass.fs
@@ -8,11 +8,7 @@ varying vec3 worldPos;
 
 void main()
 {
-  float base = 0.4;
   vec4 color = texture2D(diffuse, vec2(fTexCoord.x, fTexCoord.y));
-  vec2 viewDir = normalize(cameraPosition.xz - worldPos.xz);
-  vec2 normal = normalize(fNormal.xz);
-  color.a *= clamp(abs(dot(normal, viewDir)) * 8.0 - 0.0, 1.0, 1.0);
   if(color.a < 0.9)
     discard;
   gl_FragColor = color;
